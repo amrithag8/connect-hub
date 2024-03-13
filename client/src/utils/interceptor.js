7ed047d62645bfaebf09f630070a86a9e7aa4cf4
amrithag8 <amrithag8@gmail.com>
2024-03-13 17:42:14 +0530

    url corrected

--- client/src/utils/interceptor.js
+++ client/src/utils/interceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance=axios.create({
-    baseURL: "https://connect-hub-19ki.onrender.com/api/",
+    baseURL: "http://localhost:3007/api",
 })
 
 
